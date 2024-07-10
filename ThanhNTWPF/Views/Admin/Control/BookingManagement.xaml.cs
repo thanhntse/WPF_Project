d37f866218e770733f3761574e5c66ccb04decf0
thanhntse <nguyenthaithanh07@gmail.com>
2024-07-10 17:15:10 +0700

    feat: add admin feature

---
+++ ThanhNTWPF/Views/Admin/Control/BookingManagement.xaml.cs
@@ -0,0 +1,40 @@
+﻿using BusinessObjects.Models;
+using Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ThanhNTWPF.Views.Admin.Control
+{
+    /// <summary>
+    /// Interaction logic for BookingManagement.xaml
+    /// </summary>
+    public partial class BookingManagement : UserControl
+    {
+        private readonly IBookingReservationService BookingReservationService;
+        public List<BookingReservation> BookingReservations { get; set; }
+        public BookingManagement()
+        {
+            InitializeComponent();
+            BookingReservationService = new BookingReservationService();
+            BookingReservations = BookingReservationService.GetAll();
+            this.DataContext = this;
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
